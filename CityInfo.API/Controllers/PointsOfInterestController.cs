f2a77ffa9e111a4c94fe1175d8af68549a99af2d
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2021-07-22 18:09:50 +1000

    patch added to controller

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,9 +1,6 @@
 ﻿using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CityInfo.API.Controllers
 {
