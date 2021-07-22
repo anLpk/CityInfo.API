ab3da305dbe73ad816fb1179724cf4bbf6e6c4d6
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2021-07-23 09:02:00 +1000

    logger added

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,6 +1,10 @@
 ﻿using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CityInfo.API.Controllers
 {
@@ -8,7 +12,11 @@ namespace CityInfo.API.Controllers
     [Route("api/cities/{cityId}/pointsofinterest")]
     public class PointsOfInterestController : ControllerBase
     {
-        private object pointOfInterestFromStore;
+        private readonly ILogger<PointsOfInterestController> _logger;
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
 
         [HttpGet]
         public IActionResult GetPointsOfInterest(int cityId)
